4870ea501076644dbebe3fc7a9ba05a74c42c0c3
COL1_DEV <57326899+Steve557mag-fr@users.noreply.github.com>
2024-03-25 14:04:11 +0100

    suite

--- Q2Proto/Assets/Resources/Scripts/SpaceTravel.cs
+++ Q2Proto/Assets/Resources/Scripts/SpaceTravel.cs
@@ -1,18 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class SpaceTravel : MonoBehaviour
+public class SpaceTravel : MiniGame
 {
-    // Start is called before the first frame update
-    void Start()
+    public int goodPath;
+
+    public SpriteRenderer[] pathHints;
+    public SpriteRenderer userSelection;
+
+    public KeyCode up, middle, down;
+
+    int pathOK = 0;
+
+    public override void StartGame(GameObject area)
     {
-        
+        base.StartGame(area);
+
+        pathOK = 0;
+        NextPathsChoice();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void UpdateGame()
     {
-        
+        base.UpdateGame();
+
+        if (Input.GetKeyDown(up)) TakePath(0);
+        else if (Input.GetKeyDown(middle)) TakePath(1);
+        else if (Input.GetKeyDown(down)) TakePath(2);
+
     }
+    
+
+    public void NextPathsChoice()
+    {
+        goodPath = Random.Range(0,pathHints.Length);
+        ShowPaths();
+
+    }
+
+    void TakePath(int index)
+    {
+        if(index == goodPath)
+        {
+            pathOK++;
+            NextPathsChoice();
+        }
+        else
+        {
+            LooseGame(looseSequence);
+        }
+    }
+
+    void ShowPaths()
+    {
+        for(int i = 0; i < pathHints.Length; i++)
+        {
+            pathHints[i].color = (i == goodPath) ? Color.green : Color.red;
+        }
+    }
+
 }
